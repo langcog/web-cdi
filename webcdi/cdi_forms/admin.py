02de55bfb654a71df7e9046c0fa522435995800b
Danielle <danielle@psy-dk-md1.stanford.edu>
2016-10-25 17:40:19 -0700

    Gestures items now show up under English WG

--- webcdi/cdi_forms/admin.py
+++ webcdi/cdi_forms/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import English_WS
+from .models import English_WS, English_WG
 # Register your models here.
 
 admin.site.register(English_WS)
+admin.site.register(English_WG)
+
