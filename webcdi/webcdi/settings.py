93e46a527ea71635f096a1f85ac76b11afd15cab
Danielle <dkellier@stanford.edu>
2017-01-20 11:47:52 -0800

    Changing database password and removing from git

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from databases import *
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -111,22 +113,22 @@ WSGI_APPLICATION = 'webcdi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
 
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'webcdi-admin',                      
-            'USER': 'webcdi-admin',
-            'PASSWORD': 'first5words',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
+#     'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'webcdi-admin',                      
+#             'USER': 'webcdi-admin',
+#             'PASSWORD': 'first5words',
+#             'HOST': 'localhost',
+#             'PORT': '',
+#         }
 
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-}
+#     #'default': {
+#     #    'ENGINE': 'django.db.backends.sqlite3',
+#     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     #}
+# }
 
 
 # Internationalization
