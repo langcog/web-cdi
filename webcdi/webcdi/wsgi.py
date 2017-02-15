8bce5df16c5ef6f5bdf2f30f77280f26634cf6d9
Ubuntu <ubuntu@ip-172-31-35-28.us-west-2.compute.internal>
2017-02-15 20:30:31 +0000

    update wsgi file syspath

--- webcdi/webcdi/wsgi.py
+++ webcdi/webcdi/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+≈
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webcdi.settings")
 
 application = get_wsgi_application()
