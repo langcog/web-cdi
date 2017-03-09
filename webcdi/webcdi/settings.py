374a3561dbbb1741f8e06e54c743ef0e77ba2fd7
Ubuntu <ubuntu@ip-172-31-35-28.us-west-2.compute.internal>
2017-03-09 18:07:50 +0000

    Updating allowed_hosts. Hopefully simpler blockages of attackers

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -55,18 +55,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','52.32.108.131','.amazonaws.com','.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','52.32.108.131','webcdi-dev.us-west-2.elasticbeanstalk.com','webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu']
 
-IPS_TO_ADD = ['webcdi-dev.us-west-2.elasticbeanstalk.com', 'webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', socket.gethostname()]
-
-NEW_IPS = set()
-
-for IP in IPS_TO_ADD:
-    for i in range(0,100):
-    	NEW_IPS.add(socket.gethostbyname(IP))
-
-for IP in list(NEW_IPS):
-	ALLOWED_HOSTS.append(IP)
 
 # Application definition
 
