cff6678d33f6fbd9d24d5d64b1801750ea043bc4
Ubuntu <ubuntu@ip-172-31-35-28.us-west-2.compute.internal>
2017-03-10 00:53:36 +0000

    Update allowed_hosts

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -57,6 +57,17 @@ MANAGERS = ADMINS
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','52.32.108.131','webcdi-dev.us-west-2.elasticbeanstalk.com','webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu']
 
+IPS_TO_ADD = ['webcdi-dev.us-west-2.elasticbeanstalk.com', 'webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', socket.gethostname()]
+
+NEW_IPS = set()
+
+for IP in IPS_TO_ADD:
+    for i in range(0,100):
+    	NEW_IPS.add(socket.gethostbyname(IP))
+
+for IP in list(NEW_IPS):
+	ALLOWED_HOSTS.append(IP)
+
 # Application definition
 
 INSTALLED_APPS = (
