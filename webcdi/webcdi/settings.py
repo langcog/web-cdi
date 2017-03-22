4ab39d2c57b106e82f9b9e85ca309fc22914d214
Ubuntu <ubuntu@ip-172-31-35-28.us-west-2.compute.internal>
2017-03-22 18:48:11 +0000

    Updated site_id settings. Post-install specificity based on connected database

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -89,8 +89,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
 )
 
-SITE_ID = 1
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
