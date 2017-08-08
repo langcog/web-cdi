90e5ec79d19c6c741ba1273e4a5472f5b15f4447
Danielle <dkellier@stanford.edu>
2017-08-08 15:36:21 -0700

    Commented out supplementtut app

--- webcdi/supplementtut/models.py
+++ webcdi/supplementtut/models.py
@@ -4,13 +4,13 @@ from django.utils.encoding import python_2_unicode_compatible
 from registration.supplements.base import RegistrationSupplementBase
 
 @python_2_unicode_compatible
-class MyRegistrationSupplement(RegistrationSupplementBase):
+class MyRegistrationSupplement(RegistrationSupplementBase): # Model for supplementary questions to account registration
 
-    name = models.CharField(verbose_name = "Full name", max_length=101, help_text="Please fill your full name here")
-    institution = models.CharField(verbose_name = "Name of institution", max_length=101)
-    position = models.CharField(verbose_name = "Position in Institution", max_length=101)
-    comments = models.TextField(verbose_name = "Comments", blank=True)
+    name = models.CharField(verbose_name = "Full name", max_length=101, help_text="Please fill your full name here") # Full name of researcher or research group
+    institution = models.CharField(verbose_name = "Name of institution", max_length=101) # Name of research institution they are affiliated with
+    position = models.CharField(verbose_name = "Position in Institution", max_length=101) # Title of position within research institution
+    comments = models.TextField(verbose_name = "Comments", blank=True) # Any additional comments they wanted to add
 
     def __str__(self):
         # a summary of this supplement
-        return "%s, %s (%s)" % (self.name, self.position, self.institution)
+        return "%s, %s (%s)" % (self.name, self.position, self.institution) # Format for how to present a summary of supplementary information in staff interface
