075886097dcb43d86b98605cc012afaea4222cac
Danielle <dkellier@stanford.edu>
2017-09-11 17:01:46 -0700

    Auth template fix. Render manually for bootstrap 4 (replacement until BS4-ready crispy forms release)

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -147,29 +147,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-# LOGGING = {
-#      'version': 1,
-#      'disable_existing_loggers': False,
-#      'handlers': {
-#          'file': {
-#              'level': 'DEBUG',
-#              'class': 'logging.FileHandler',
-#              'filename': os.path.join(BASE_DIR, '../django_logs/debug.log'),
-#          },
-#      },
-#      'loggers': {
-#          'django.request': {
-#              'handlers': ['file'],
-#              'level': 'DEBUG',
-#              'propagate': True,
-#          },
-#      },
-#  }
 
 LOGGING = {
     'version': 1,
