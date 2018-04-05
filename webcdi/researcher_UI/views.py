62ba8054f4b5f2b3e1023d2fb255d7b379ce6e9c
Danielle <dkellier@stanford.edu>
2018-04-05 12:41:01 -0700

    Import function can accept optional demographic variables. Strict set of allowable values. Spreadsheet of value mappings to come

--- webcdi/researcher_UI/views.py
+++ webcdi/researcher_UI/views.py
@@ -10,7 +10,7 @@ from .tables  import StudyAdministrationTable
 from django_tables2   import RequestConfig
 from django.db.models import Max
 from cdi_forms.views import model_map, get_model_header, background_info_form, prefilled_background_form
-from cdi_forms.models import BackgroundInfo
+from cdi_forms.models import BackgroundInfo, Zipcode
 from django.utils.encoding import force_text
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib.auth.models import User
@@ -686,22 +686,113 @@ def try_parsing_date(text):
             pass
     raise ValueError('no valid date format found')
 
+def make_unicode(s):
+    if type(s) != unicode:
+        s =  s.decode('utf-8')
+        return s
+    else:
+        return s
+
+def processDemos(csv_file, demo_list = None):
+    recoded_df = csv_file.where((pd.notnull(csv_file)), None)
+    PROJECT_ROOT = settings.BASE_DIR
+
+    if demo_list:
+        for col in demo_list:
+            if 'boolean' in col or col == 'born_on_due_date':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in [0,1] else 2)
+            elif col == 'child_hispanic_latino':
+                recoded_df[col] = recoded_df[col].apply(lambda x: bool(x) if x in [0,1] else None)
+            elif col == 'birth_order':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(1,10) else 0).astype('int')
+            elif col in ['ear_infections', 'hearing_loss', 'illnesses', 'learning_disability', 'multi_birth', 'services', 'vision_problems', 'worried']:
+                recoded_df[col] = recoded_df[col].apply(lambda x: make_unicode(x) if x and len(x) <= 1000 else None)
+            elif col == 'language_from':
+                recoded_df[col] = recoded_df[col].apply(lambda x: make_unicode(x) if x and len(x) <= 50 else None)
+            elif 'yob' in col:
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(1950, datetime.datetime.today().year + 1) else 0).astype('int')
+            elif 'education' in col:
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(1,24) else 0).astype('int')
+            elif col == 'early_or_late':
+                recoded_df[col] = recoded_df[col].apply(lambda x: make_unicode(x.lower()) if x and x.lower() in ['early', 'late'] else None)
+            elif col == 'zip_code':
+                def parse_zipcode(c):
+                    if c and re.match("(\d{3}([*]{2})?)", c):
+                        if Zipcode.objects.filter(zip_prefix = c).exists():
+                            return Zipcode.objects.filter(zip_prefix = c).first().state
+                        else:
+                            return c + '**'
+                    elif c and re.match("([A-Z]{2})", c):
+                        return make_unicode(c)
+                    else:
+                        return u''
+                recoded_df[col] = recoded_df[col].str[:3].apply(parse_zipcode)
+            elif col == 'language_days_per_week':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(1,8) else None)
+            elif col == 'language_hours_per_day':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(1,25) else None)
+            elif col == 'due_date_diff':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x >= 1 else None)
+            elif col == 'caregiver_info':
+                recoded_df[col] = recoded_df[col].apply(lambda x: int(x) if x in range(0,5) else 0)
+            elif col == 'birth_weight_lb':
+                def round_lb(c):
+                    c = int(c * 2) / 2.0
+                    c = 1.0 if c < 3.0 else c
+                    c = 10.0 if c > 10.0 else c
+                    return c 
+                recoded_df[col] = recoded_df[col].apply(round_lb)
+            elif col == 'birth_weight_kg':
+                def round_kg(c):
+                    c = int(c * 4) / 4.0
+                    c = 1.0 if c < 1.5 else c
+                    c = 5.0 if c > 5.0 else c
+                    return c 
+                recoded_df[col] = recoded_df[col].apply(round_kg)
+            elif col == 'annual_income':
+                recoded_df[col] = recoded_df[col].apply(lambda x: make_unicode(x) if x and x in dict(BackgroundInfo._meta.get_field(col).choices).keys() else u'Prefer not to disclose')                 
+            elif col == 'child_ethnicity':
+                recoded_df[col] = recoded_df[col].apply(lambda x: list(set(make_unicode(x).upper().split('/')) & set([u'N', u'H', u'W', u'B', u'A', u'O'])) if x else [])
+            elif col == 'other_languages':
+                lang_choices = [make_unicode(v['name']) for k,v in json.load(codecs.open(PROJECT_ROOT + '/languages.json', 'r', 'utf-8')).iteritems()]
+                recoded_df[col] = recoded_df[col].apply(lambda x: list(set([y.strip() for y in make_unicode(x).split('/')]) & set(lang_choices)) if x else [])
+
+    recoded_df = recoded_df.where((pd.notnull(recoded_df)), None)
+    return recoded_df
+
 def importFillablePDFs(request, study_name):
 
     study_obj = study.objects.get(researcher= request.user, name= study_name)
 
     PROJECT_ROOT = settings.BASE_DIR
     instruments_json = json.load(open(os.path.realpath(PROJECT_ROOT + '/static/json/instruments.json')))
-
     header_file_path = filter(lambda x: x['language'] == study_obj.instrument.language and x['form'] == study_obj.instrument.form, instruments_json)[0]['fillable_headers']
-
     pdf_header_df = pd.read_csv(header_file_path)
 
+    default_dict = {
+    'annual_income': u'Prefer not to disclose', 'birth_order': 0, 'birth_weight_kg': None,
+    'birth_weight_lb': 0.0, 'born_on_due_date': 2, 'caregiver_info': 0,
+    'child_ethnicity': [], 'child_hispanic_latino': None, 'due_date_diff': None,
+    'ear_infections': None, 'ear_infections_boolean': 2, 'early_or_late': u'',
+    'father_education': 0, 'father_yob': 0, 'hearing_loss': None,
+    'hearing_loss_boolean': 2, 'illnesses': None, 'illnesses_boolean': 2,
+    'language_days_per_week': None, 'language_from': None, 'language_hours_per_day': None,
+    'learning_disability': None, 'learning_disability_boolean': 2, 'mother_education': 0,
+    'mother_yob': 0, 'multi_birth': None, 'multi_birth_boolean': 2,
+    'other_languages': [], 'other_languages_boolean': 2, 'services': None,
+    'services_boolean': 2, 'vision_problems': None, 'vision_problems_boolean': 2,
+    'worried': None, 'worried_boolean': 2, 'zip_code': u''
+    }
+
     csv_file = pd.read_csv(request.FILES['imported-file'])
+    demo_list = list(set(default_dict.keys()) & set(csv_file))
+
+    csv_file = processDemos(csv_file, demo_list)
 
     admin_row = next(csv_file.iterrows())[1]
     error_msg = None
     new_admin_pks = []
+
 
     for index, admin_row in csv_file.iterrows():
 
@@ -741,47 +832,16 @@ def importFillablePDFs(request, study_name):
         raw_age = dot - dob
         age = int(float(raw_age.days)/(365.2425/12.0))
 
-        background_dict = {
+        csv_dict = {
         'sex': sex,
-        'age': age,
-        'mother_yob': 0, 
-        'illnesses': None, 
-        'hearing_loss': None, 
-        'birth_weight_lb': 0.0, 
-        'language_from': None, 
-        'father_yob': 0,  
-        'multi_birth': None, 
-        'due_date_diff': None, 
-        'language_hours_per_day': None, 
-        'illnesses_boolean': 2, 
-        'ear_infections': None, 
-        'other_languages': [], 
-        'ear_infections_boolean': 2, 
-        'caregiver_info': 0, 
-        'other_languages_boolean': 2, 
-        'services_boolean': 2, 
-        'learning_disability': None, 
-        'birth_order': 0, 
-        'born_on_due_date': 2, 
-        'zip_code': u'', 
-        'birth_weight_kg': None, 
-        'mother_education': 0, 
-        'multi_birth_boolean': 2, 
-        'vision_problems_boolean': 2, 
-        'early_or_late': u'', 
-        'services': None, 
-        'language_days_per_week': None, 
-        'hearing_loss_boolean': 2, 
-        'annual_income': u'Prefer not to disclose', 
-        'learning_disability_boolean': 2,  
-        'child_ethnicity': [], 
-        'worried': None, 
-        'vision_problems': None, 
-        'father_education': 0, 
-        'worried_boolean': 2, 
-        'child_hispanic_latino': None
+        'age': age
         }
-        
+
+        background_dict = default_dict.copy()
+        background_dict.update(csv_dict)
+
+        if demo_list:
+            background_dict.update(dict(admin_row[demo_list]))
 
         try:
             new_background, created = BackgroundInfo.objects.get_or_create(administration = new_admin, defaults = background_dict)
