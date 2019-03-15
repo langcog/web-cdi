a1c8c5ef5864970272752b37a63709eb98d5c1f6
HenryMehta <hjsmehta@gmail.com>
2019-03-15 14:54:11 +0000

    Issue123: Email registration form

---
+++ webcdi/researcher_UI/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'researcher_UI.apps.ResearcherUIAppConfig'
---
+++ webcdi/researcher_UI/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+from django.utils.translation import ugettext_lazy as _
+
+class ResearcherUIAppConfig(AppConfig):
+    name = 'researcher_UI'
+
+    def ready(self):
+        import researcher_UI.signals
