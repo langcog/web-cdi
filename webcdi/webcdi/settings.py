7879f605321196ae74bb5706c4fae2cdfee8c8dc
HenryMehta <hjsmehta@gmail.com>
2019-06-06 13:14:29 +0100

    add web-cdi-dev2

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -60,9 +60,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-ALLOWED_HOSTS = ['http://web-cdi-dev2.mfpemr5vcz.us-west-2.elasticbeanstalk.com/','localhost', '127.0.0.1','52.32.108.131','webcdi-dev.us-west-2.elasticbeanstalk.com','webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', 'webcdi-dev.stanford.edu', '.elb.amazonaws.com']
+ALLOWED_HOSTS = ['web-cdi-dev2.mfpemr5vcz.us-west-2.elasticbeanstalk.com','localhost', '127.0.0.1','52.32.108.131','webcdi-dev.us-west-2.elasticbeanstalk.com','webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', 'webcdi-dev.stanford.edu', '.elb.amazonaws.com']
 
-IPS_TO_ADD = ['webcdi-dev.us-west-2.elasticbeanstalk.com', 'webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', socket.gethostname()]
+IPS_TO_ADD = ['web-cdi-dev2.mfpemr5vcz.us-west-2.elasticbeanstalk.com','webcdi-dev.us-west-2.elasticbeanstalk.com', 'webcdi-prod.us-west-2.elasticbeanstalk.com', 'webcdi.stanford.edu', socket.gethostname()]
 
 NEW_IPS = set()
 
