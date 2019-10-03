f80570894a6ca860a05a63db299060a942b06652
Henry Mehta <hjsmehta@gmail.com>
2019-10-03 12:18:03 +0100

    Waiver RichText (#185)
    Ensuring Add Participant modal works

--- webcdi/researcher_UI/static/researcher_UI/django_modal_forms.js
+++ webcdi/researcher_UI/static/researcher_UI/django_modal_forms.js
@@ -8,35 +8,40 @@ function modal_form(form_url){
             $('#'+modal_id).html(data);
             $('#'+modal_id).modal('show');
             var callback = function() {
-                for (var instance in CKEDITOR.instances) {
-                    CKEDITOR.instances[instance].updateElement();
-                }
-                $.ajax({
-                    type: 'POST',
-                    url: form_url,
-                    data: new FormData($('#'+modal_id+" form")[0]),
-                    cache: false,
-                    contentType: false,
-                    processData: false,
-
-                    success: function (data, status) {
-                        if (data['stat'] == "ok") {
-                            $('#'+modal_id).modal('hide');
-                            $('#'+modal_id).children().remove();
-                            window.location = data['redirect_url']
-
-                        }
-                        else if(data['stat'] == "re-render"){
-                            $('#'+modal_id).html(data);
-                            $('#'+modal_id).modal('show');
-                        }
-                        else if(data['stat'] == "error"){
-                            $('#'+modal_id).modal('show');
-                            $('#'+modal_id +' #error-message-text').html(data['error_message']);
-                            $('#'+modal_id + ' .error-message').css('display','block');
-                        }
+                try {
+                    for (var instance in CKEDITOR.instances) {
+                        CKEDITOR.instances[instance].updateElement();
                     }
-                });                
+                }   
+                catch (err){}
+                finally {
+                    $.ajax({
+                        type: 'POST',
+                        url: form_url,
+                        data: new FormData($('#'+modal_id+" form")[0]),
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+
+                        success: function (data, status) {
+                            if (data['stat'] == "ok") {
+                                $('#'+modal_id).modal('hide');
+                                $('#'+modal_id).children().remove();
+                                window.location = data['redirect_url']
+
+                            }
+                            else if(data['stat'] == "re-render"){
+                                $('#'+modal_id).html(data);
+                                $('#'+modal_id).modal('show');
+                            }
+                            else if(data['stat'] == "error"){
+                                $('#'+modal_id).modal('show');
+                                $('#'+modal_id +' #error-message-text').html(data['error_message']);
+                                $('#'+modal_id + ' .error-message').css('display','block');
+                            }
+                        }
+                    });  
+                }              
             };
             $("#"+modal_id+" input").keypress(function() {
                 console.log(event.which)
