7fa0962a766d2838e869504912aeadfe645bd892
HenryMehta <hjsmehta@gmail.com>
2019-12-11 18:52:08 +0000

    Hebrew

--- webcdi/cdi_forms/views.py
+++ webcdi/cdi_forms/views.py
@@ -718,8 +718,8 @@ def printable_view(request, hash_id):
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
 
     if administration_instance.study.researcher.username == "langcoglab" and administration_instance.study.allow_payment:
-        #response.set_signed_cookie('completed_num',str(completed))
-        response.set_signed_cookie('completed_num',bytes(completed))
+        response.set_signed_cookie('completed_num',str(completed))
+        #response.set_signed_cookie('completed_num',bytes(completed))
     return response
 
 
