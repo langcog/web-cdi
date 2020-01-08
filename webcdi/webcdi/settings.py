473ae6e43836465e78ddaa9067277e3802a4489d
HenryMehta <hjsmehta@gmail.com>
2020-01-08 08:46:29 +0000

    Issue 210 - Simplify Unique Settings

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -17,9 +17,6 @@ from .secret_settings import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -38,50 +35,15 @@ except ImportError:
     generate_secret_key(os.path.join(SETTINGS_DIR, 'secret_key.py'))
     from .secret_key import *
 
-#SECRET_KEY = 'bt8n72d_ks+(7d-s&9%3g^b(m3g#_grs#ir!m-p5$^5se@fdsu'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = bool(os.environ.get('DEBUG', False))
 TEMPLATE_DEBUG = False
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 
-ADMINS = (
-    ('Henry Mehta', 'hjsmehta@gmail.com'),
-)
-
 MANAGERS = ADMINS
 
-ALLOWED_HOSTS = [
-    'webcdi-prod-py36.us-west-2.elasticbeanstalk.com',
-    'webcdi-dev-py36.us-west-2.elasticbeanstalk.com', 
-    'ec2-34-208-105-171.us-west-2.compute.amazonaws.com',
-    'ec2-52-88-52-34.us-west-2.compute.amazonaws.com',
-    'web-cdi-dev6.mfpemr5vcz.us-west-2.elasticbeanstalk.com',
-    'web-cdi-dev8.mfpemr5vcz.us-west-2.elasticbeanstalk.com',
-    'localhost', 
-    '127.0.0.2',
-    '127.0.0.1',
-    'webcdi-dev.us-west-2.elasticbeanstalk.com',
-    'webcdi-prod.us-west-2.elasticbeanstalk.com', 
-    'webcdi.stanford.edu', 
-    'webcdi-dev.stanford.edu', 
-    '.elb.amazonaws.com']
-
-IPS_TO_ADD = [socket.gethostname()]
-
-NEW_IPS = set()
-
-for IP in IPS_TO_ADD:
-    for i in range(0,100):
-    	NEW_IPS.add(socket.gethostbyname(IP))
-
-for IP in list(NEW_IPS):
-	ALLOWED_HOSTS.append(IP)
-
 # Application definition
-
 INSTALLED_APPS = (
     'researcher_UI', 
     'modeltranslation',
@@ -157,11 +119,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webcdi.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -178,7 +135,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
@@ -259,8 +216,7 @@ COUNTRIES_FIRST = [
     'US','CA','NL'
 ]
 
-if SITE_ID == 3: USER_ADMIN_EMAIL = 'henrymehta@hotmail.com'
-else : USER_ADMIN_EMAIL = 'webcdi-contact@stanford.edu'
+
 
 #CSRF_COOKIE_SECURE=False
 #CSRF_TRUSTED_ORIGINS = ('.elasticbeanstalk.com',)
