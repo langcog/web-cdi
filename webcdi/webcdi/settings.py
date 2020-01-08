0be04025f2168a56eb257fae0d1e3d3f6d8be61e
HenryMehta <hjsmehta@gmail.com>
2020-01-08 08:48:18 +0000

    Issue 210 - Simplify Unique Settings

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.utils.translation import gettext_lazy as _
-from .databases import *
+from .secret_settings import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
