bb1793a6061593c4c173b8258deb874cb839d957
HenryMehta <hjsmehta@gmail.com>
2020-01-21 14:11:54 +0000

    cron changes

--- webcdi/researcher_UI/management/commands/update_summary_data.py
+++ webcdi/researcher_UI/management/commands/update_summary_data.py
@@ -1,16 +1,26 @@
 from django.core.management.base import BaseCommand
 from cdi_forms.scores import update_summary_scores
 import datetime
-from researcher_UI.models import administration
+from researcher_UI.models import administration, study
 
 from django.conf import settings
 from django.core.mail import EmailMessage
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-s', '--study_id', type=int)
 
     def handle(self, *args, **options):
+        if options['study_id']:
+            study_obj = study.objects.get(pk=options['study_id'])
+            subject = f'WebCDI Summary Data for %s' % study_obj.name
+            administrations = administration.objects.filter(study=study_obj)
+        else: 
+            subject = f'WebCDI Summary Data'
+            administrations = administration.objects.all()
+        
         email = EmailMessage(
-                    subject="WebCDI Summary Data",
+                    subject=subject,
                     body=f'Starting at %s' % (datetime.datetime.now()),
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     to=['hjsmehta@gmail.com'],
@@ -18,16 +28,24 @@ class Command(BaseCommand):
         email.send()
         count = 0
         thousands = 0
-        for instance in administration.objects.all():
+        for instance in administrations:
             count += 1
             update_summary_scores(instance)
             if count == 1000:
                 thousands += 1
                 email = EmailMessage(
-                    subject="WebCDI Summary Data",
+                    subject=subject,
                     body=f'%s of %s records processed at %s' % (thousands * count, len(administration.objects.all()), datetime.datetime.now()),
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     to=['hjsmehta@gmail.com'],
                 )
                 email.send()
                 count = 0
+
+        email = EmailMessage(
+                    subject=subject,
+                    body=f'COMPLETED',
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    to=['hjsmehta@gmail.com'],
+                )
+        email.send()
