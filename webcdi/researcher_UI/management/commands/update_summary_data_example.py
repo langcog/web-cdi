50fc8d0ae29447fbce8f7e1edce43068dfb104a9
HenryMehta <hjsmehta@gmail.com>
2020-01-21 07:57:30 +0000

    added cron for update

--- webcdi/researcher_UI/management/commands/update_summary_data_example.py
+++ webcdi/researcher_UI/management/commands/update_summary_data_example.py
@@ -3,6 +3,9 @@ from cdi_forms.scores import update_summary_scores
 import datetime
 from researcher_UI.models import administration
 
+from django.conf import settings
+from django.core.mail import EmailMessage
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -14,5 +17,11 @@ class Command(BaseCommand):
             update_summary_scores(instance)
             if count == 10:
                 thousands += 1
-                print (f'%s of %s records processed at %s' % (thousands * count, len(administration.objects.all()), datetime.datetime.now()))
+                email = EmailMessage(
+                    subject="WebCDI Summary Data",
+                    body=f'%s of %s records processed at %s' % (thousands * count, len(administration.objects.all()), datetime.datetime.now()),
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    to=['hjsmehta@gmail.com'],
+                )
+                email.send()print (f'%s of %s records processed at %s' % (thousands * count, len(administration.objects.all()), datetime.datetime.now()))
                 count = 0
