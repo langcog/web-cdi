9e7ca5814372a7c2f38ee6bf785dfbda958a0c60
HenryMehta <hjsmehta@gmail.com>
2020-02-12 09:56:56 +0000

    summary scores

--- webcdi/researcher_UI/views.py
+++ webcdi/researcher_UI/views.py
@@ -39,6 +39,7 @@ from cdi_forms.models import Instrument_Forms
 def get_study_scores(administrations):
     scores = SummaryData.objects.values('administration_id', 'title','value').filter(administration_id__in = administrations)
     melted_scores = pd.DataFrame.from_records(scores).pivot(index='administration_id', columns='title', values='value')
+    #melted_scores.fillna(0, inplace=True)
     melted_scores.reset_index(level=0, inplace=True)
     return melted_scores
 
