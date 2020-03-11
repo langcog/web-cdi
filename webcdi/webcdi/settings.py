96cb5faad1a4d92b5827f31cc37d628f45589c1f
HenryMehta <hjsmehta@gmail.com>
2020-03-11 09:31:38 +0000

    Issues 238-240

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -151,6 +151,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'webcdi.context_processors.home_page',
             ],
         },
     },
