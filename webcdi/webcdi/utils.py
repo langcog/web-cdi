07db91b90653fdc54eef629da33842e35992f7a8
HenryMehta <hjsmehta@gmail.com>
2020-03-11 10:29:57 +0000

    Trying to stop health check error

---
+++ webcdi/webcdi/utils.py
@@ -0,0 +1,25 @@
+import os
+import urllib.request
+
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        return response.read()
+    except:
+        return None
+    finally:
+        if response:
+            response.close()
