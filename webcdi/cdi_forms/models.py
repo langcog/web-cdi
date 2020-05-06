1434af44608b6e4dc8a691d19952143375ec61ba
HenryMehta <hjsmehta@gmail.com>
2020-05-06 11:30:51 +0100

    Cielke Changes

--- webcdi/cdi_forms/models.py
+++ webcdi/cdi_forms/models.py
@@ -67,7 +67,7 @@ class BackgroundInfo(models.Model):
     age = models.IntegerField(verbose_name = _("Age (in months)"), validators=[MinValueValidator(0)], default = 999) #age in months for child (views.py converts DOB field in forms.py into age for this model)
     sex = models.CharField(max_length = 1, blank=True, null=True, choices = (('M', _("Male")), ('F', _("Female")), ('O', _("Other")))) # Reported gender for child
     country = CountryField(verbose_name=_("Country"), blank = True, null=True)
-    zip_code = models.CharField(max_length = 5, verbose_name = _('Zip Code (if you live in the U.S.)'), blank = True, null=True, validators=[RegexValidator(regex='^(\d{3}([*]{2})?)|([A-Z]{2})$', message=_('Please enter a valid U.S. zip code'))]) # Reported zip code for family. Follows Safe Harbor guidelines. Stores first 3 digits of zip code or state abbreviation.
+    zip_code = models.CharField(max_length = 6, verbose_name = _('Zip Code (if you live in the U.S.)'), blank = True, null=True, validators=[RegexValidator(regex='^(\d{3}([*]{2})?)|([A-Z]{2}|\d{4}([A-Z]{2}}))$', message=_('Please enter a valid U.S. zip code'))]) # Reported zip code for family. Follows Safe Harbor guidelines. Stores first 3 digits of zip code or state abbreviation.
 
     #Declared set of choices for birth order. Displays text version of integer ("First" instead of 1)
     birth_order_choices = [
