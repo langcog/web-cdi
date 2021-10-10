d3c2afce6e55ccd44d92ac6abffd25d70b7748b5
Henry Mehta <hjsmehta@gmail.com>
2021-10-10 17:15:29 +0100

    amend django.sh

--- webcdi/.platform/hooks/postdeploy/02_django.sh
+++ webcdi/.platform/hooks/postdeploy/02_django.sh
@@ -2,12 +2,12 @@
 
 source "$PYTHONPATH/activate" && {
     # migrate
-    python ./webcdi/manage.py migrate --noinput;
-    python ./webcdi/manage.py collectstatic --noinput;
-    python ./webcdi/manage.py populate_instrument;
-    python ./webcdi/manage.py populate_scoring;
-    python ./webcdi/manage.py populate_benchmark
-    python ./webcdi/manage.py populate_choices;
-    python ./webcdi/manage.py populate_items;
-    python ./webcdi/manage.py populate_cat_items;
+    python ./manage.py migrate --noinput;
+    python ./manage.py collectstatic --noinput;
+    python ./manage.py populate_instrument;
+    python ./manage.py populate_scoring;
+    python ./manage.py populate_benchmark
+    python ./manage.py populate_choices;
+    python ./manage.py populate_items;
+    python ./manage.py populate_cat_items;
 }
