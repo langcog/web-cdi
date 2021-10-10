703e9c907911ed9063e1a8ef0e93fa316e5e2a24
Henry Mehta <hjsmehta@gmail.com>
2021-10-10 16:20:01 +0100

    move elasticbeanstalk to webcdi

---
+++ webcdi/.platform/hooks/postdeploy/02_django.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source "$PYTHONPATH/activate" && {
+    # migrate
+    python ./webcdi/manage.py migrate --noinput;
+    python ./webcdi/manage.py collectstatic --noinput;
+    python ./webcdi/manage.py populate_instrument;
+    python ./webcdi/manage.py populate_scoring;
+    python ./webcdi/manage.py populate_benchmark
+    python ./webcdi/manage.py populate_choices;
+    python ./webcdi/manage.py populate_items;
+    python ./webcdi/manage.py populate_cat_items;
+}
