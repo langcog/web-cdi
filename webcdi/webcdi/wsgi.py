a0c2be307ad4a70840d52543b94dcdaaaa100b0f
Henry Mehta <hjsmehta@gmail.com>
2021-10-11 09:07:45 +0100

    py38

--- webcdi/webcdi/wsgi.py
+++ webcdi/webcdi/wsgi.py
@@ -15,6 +15,6 @@ from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webcdi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webcdi.webcdi.settings")
 
 application = get_wsgi_application()
