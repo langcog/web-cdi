03e2746166c989b4508bc006c83db329d2abd673
Henry Mehta <hjsmehta@gmail.com>
2021-10-11 07:51:45 +0100

    py38

--- .platform/hooks/postdeploy/02_django.sh
+++ .platform/hooks/postdeploy/02_django.sh
@@ -4,9 +4,9 @@ source "$PYTHONPATH/activate" && {
     # migrate
     python ./webcdi/manage.py migrate --noinput;
     python ./webcdi/manage.py collectstatic --noinput;
-    python ./webcdi/manage.py populate_instrument;
-    python ./webcdi/manage.py populate_scoring;
-    python ./webcdi/manage.py populate_benchmark
-    python ./webcdi/manage.py populate_choices;
-    python ./webcdi/manage.py populate_items;
+    #python ./webcdi/manage.py populate_instrument;
+    #python ./webcdi/manage.py populate_scoring;
+    #python ./webcdi/manage.py populate_benchmark
+    #python ./webcdi/manage.py populate_choices;
+    #python ./webcdi/manage.py populate_items;
 }
