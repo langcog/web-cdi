3c4a4287ec7f6175d1ad00ad65db5b800a4964f6
henrymehta <hjsmehta@gmail.com>
2022-12-19 16:56:22 +0000

    Remove broken link

--- webcdi/webcdi/settings.py
+++ webcdi/webcdi/settings.py
@@ -92,7 +92,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    #'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
