d2a9141d559cb17a7e079c31ea5e0375463c46ce
endalkh <endalkhbel@gmail.com>
2022-12-30 16:12:17 +0300

    remove: links on tables

--- webcdi/researcher_UI/tables.py
+++ webcdi/researcher_UI/tables.py
@@ -7,21 +7,49 @@ from django.utils.html import mark_safe
 
 # Table for organizing administration objects into a table on the researcher interface
 class StudyAdministrationTable(tables.Table):
-    #select_col = tables.columns.CheckBoxColumn(accessor='id') # Creates a column of checkboxes associated with each administration ID
-    select_col = tables.CheckBoxColumn(accessor="pk", attrs = { "th__input": 
-                                        {"onclick": "toggle(this)"}},
-                                        orderable=False)
-    subject_id = tables.TemplateColumn('<a href="/interface/edit-administration/{{ record.pk }}/">{{ record.subject_id }}</a>') #Generate Link to edit subject_id
-    local_lab_id = tables.TemplateColumn('<a href="/interface/edit-local-lab-id/{{ record.pk }}/">{{ record.local_lab_id }}</a>') #Generate Link to edit local_lab_id    
-    link = tables.TemplateColumn('<a href="{{ record.get_absolute_url }}" target="_blank">link</a>', orderable=False) # Generates a column of administration links in each row
-    analysis = tables.Column(orderable=True, order_by=['analysis','pk'])
-    opt_out = tables.TemplateColumn('<a href="/interface/edit-opt-out/{{ record.pk }}/">{{ record.opt_out }}</a>')
-    
+    # select_col = tables.columns.CheckBoxColumn(accessor='id') # Creates a column of checkboxes associated with each administration ID
+    select_col = tables.CheckBoxColumn(
+        accessor="pk", attrs={"th__input": {"onclick": "toggle(this)"}}, orderable=False
+    )
+    subject_id = tables.TemplateColumn(
+        '<span href="/interface/edit-administration/{{ record.pk }}/">{{ record.subject_id }}</span>'
+    )  # Generate Link to edit subject_id
+    local_lab_id = tables.TemplateColumn(
+        '<span href="/interface/edit-local-lab-id/{{ record.pk }}/">{{ record.local_lab_id }}</span>'
+    )  # Generate Link to edit local_lab_id
+    link = tables.TemplateColumn(
+        '<a href="{{ record.get_absolute_url }}" target="_blank">link</a>',
+        orderable=False,
+    )  # Generates a column of administration links in each row
+    analysis = tables.Column(orderable=True, order_by=["analysis", "pk"])
+    opt_out = tables.TemplateColumn(
+        '<span href="/interface/edit-opt-out/{{ record.pk }}/">{{ record.opt_out }}</span>'
+    )
+
     def render_analysis(self, value):
-        return mark_safe('<span class="true">✔</span>') if value else mark_safe('<span class="false">✘</span>') if not value else ''
+        return (
+            mark_safe('<span class="true">✔</span>')
+            if value
+            else mark_safe('<span class="false">✘</span>')
+            if not value
+            else ""
+        )
 
     # Associates administration table with administration model
     class Meta:
         model = administration
-        exclude = ("study",'id', 'url_hash', 'page_number', 'bypass', 'include') # Excludes some fields in administration objects from table
-        sequence = ('select_col','subject_id', 'local_lab_id', 'repeat_num', 'link') # Specifies column order within table
+        exclude = (
+            "study",
+            "id",
+            "url_hash",
+            "page_number",
+            "bypass",
+            "include",
+        )  # Excludes some fields in administration objects from table
+        sequence = (
+            "select_col",
+            "subject_id",
+            "local_lab_id",
+            "repeat_num",
+            "link",
+        )  # Specifies column order within table
