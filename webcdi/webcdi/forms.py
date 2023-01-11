23dc81ea63c334cba718de9f002a23715ef72b84
endalkh <endalkhbel@gmail.com>
2023-01-11 15:56:42 +0300

    fix: issue #2

--- webcdi/webcdi/forms.py
+++ webcdi/webcdi/forms.py
@@ -4,12 +4,23 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    institution = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    position = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=False, help_text="Optional.")
+    last_name = forms.CharField(max_length=30, required=False, help_text="Optional.")
+    email = forms.EmailField(
+        max_length=254, help_text="Required. Inform a valid email address."
+    )
+    institution = forms.CharField(max_length=30, required=False, help_text="Optional.")
+    position = forms.CharField(max_length=30, required=False, help_text="Optional.")
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'institution', 'institution', 'password1', 'password2', )
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "institution",
+            "position",
+            "password1",
+            "password2",
+        )
