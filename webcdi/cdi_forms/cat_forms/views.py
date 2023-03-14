dcd0668b445dbb4e3f00dd96ac3edf88e7e33933
Henry Mehta <hjsmehta@gmail.com>
2023-03-14 19:17:35 +0000

    Easiest (#432)
    * Amend CAT Easiest and Hardest process
    
    * Amend CAT Easiest and Hardest process
    
    * Amend CAT Easiest and Hardest process
    
    * Amend CAT Easiest and Hardest process
    
    * Amend CAT Easiest and Hardest process
    
    * Change Easiest and Hardest so correct in CAT completed
    
    * Change Easiest and Hardest so correct in CAT completed
    
    * Reinstate Download Links
    
    * Upgrade apps

--- webcdi/cdi_forms/cat_forms/views.py
+++ webcdi/cdi_forms/cat_forms/views.py
@@ -1,154 +1,202 @@
+import logging
 import os.path
 
-from django.shortcuts import render, redirect
-from django.views.generic import UpdateView
-from django.http import Http404
-from django.utils import timezone, translation
-from django.db.models import Min
+from cdi_forms.models import BackgroundInfo, requests_log
+from cdi_forms.views import (
+    PROJECT_ROOT,
+    BackgroundInfoView,
+    BackpageBackgroundInfoView,
+    CreateBackgroundInfoView,
+    language_map,
+)
 from django.conf import settings
-
-from cdi_forms.models import requests_log, BackgroundInfo
-from cdi_forms.views import BackgroundInfoView, CreateBackgroundInfoView, BackpageBackgroundInfoView, PROJECT_ROOT, language_map
+from django.db.models import Min
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.utils import timezone, translation
+from django.views.generic import UpdateView
 from researcher_UI.models import administration
 
+from .cdi_cat_api import cdi_cat_api
 from .forms import CatItemForm
 from .models import CatResponse
 from .utils import string_bool_coerce
-from .cdi_cat_api import cdi_cat_api
 
-import logging
 # Get an instance of a logger
 logger = logging.getLogger("debug")
 
 CAT_LANG_DICT = {
-    'en' : 'EN',
-    'es' : 'SP',
-    'fr' : 'FR',
-    'fr-ca' : 'FR',
-    'en-ca' : 'EN',
+    "en": "EN",
+    "es": "SP",
+    "fr": "FR",
+    "fr-ca": "FR",
+    "en-ca": "EN",
 }
 
 # Create your views here.
 
+
 class CATBackgroundInfoView(BackgroundInfoView):
     pass
+
 
 class CATCreateBackgroundInfoView(CreateBackgroundInfoView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
+
 class CATBackpageBackgroundInfoView(BackpageBackgroundInfoView):
     pass
+
 
 class AdministerAdministraionView(UpdateView):
     model = administration
     refresh = False
     hash_id = None
     form_class = CatItemForm
-    template_name = 'cdi_forms/cat_forms/cat_form.html'
-    word=None
+    template_name = "cdi_forms/cat_forms/cat_form.html"
+    word = None
     instrument_items = None
     max_words = 50
     min_words = 20
     min_error = 0.15
     est_theta = None
 
+    def get_yes_responses(self):
+        yes_list = []
+        yes_list = [
+            x
+            for x, y in zip(
+                self.object.catresponse.administered_items,
+                self.object.catresponse.administered_responses,
+            )
+            if y
+        ]
+        return yes_list
+
     def get_hardest_easiest(self):
-        if self.object.catresponse.administered_items :
-            hardest = cdi_cat_api(f'hardestWord?items={self.object.catresponse.administered_items}&language={CAT_LANG_DICT[self.language]}')['definition']
-            easiest = cdi_cat_api(f'easiestWord?items={self.object.catresponse.administered_items}&language={CAT_LANG_DICT[self.language]}')['definition']
-        else : 
+        if self.object.catresponse.administered_items:
+            yes_list = self.get_yes_responses()
+            logger.debug(f"Yes List: {yes_list}")
+            hardest = cdi_cat_api(
+                f"hardestWord?items={yes_list}&language={CAT_LANG_DICT[self.language]}"
+            )["definition"]
+            easiest = cdi_cat_api(
+                f"easiestWord?items={yes_list}&language={CAT_LANG_DICT[self.language]}"
+            )["definition"]
+        else:
             hardest = None
             easiest = None
+        logger.debug(f"Hardest: {hardest}; Easiest: {easiest}")
         return hardest, easiest
 
     def get_object(self, queryset=None):
         try:
-            self.hash_id = self.kwargs['hash_id']
+            self.hash_id = self.kwargs["hash_id"]
             obj = administration.objects.get(url_hash=self.hash_id)
         except Exception as e:
-            logger.debug(f"Obj for { self.kwargs['hash_id'] } not found with error { e }")
+            logger.debug(
+                f"Obj for { self.kwargs['hash_id'] } not found with error { e }"
+            )
             raise Http404("Administration not found")
         return obj
 
     def post(self, request, *args, **kwargs):
-        logger.debug(f'Administer CAT Form POST')
+        logger.debug(f"Administer CAT Form POST")
         self.object = self.get_object()
-        if 'btn-back' in request.POST:
-            return redirect('cat_forms:background-info', pk=self.object.backgroundinfo.id)
+        if "btn-back" in request.POST:
+            return redirect(
+                "cat_forms:background-info", pk=self.object.backgroundinfo.id
+            )
 
         administered_responses = self.object.catresponse.administered_responses or []
         administered_words = self.object.catresponse.administered_words or []
         administered_items = self.object.catresponse.administered_items or []
-        
-        self.word = {'index': self.request.POST['word_id'], 'definition' : self.request.POST['label']}
-        
-        administered_words.append(self.word['definition'])
-        if 'yes' in self.request.POST:
+
+        self.word = {
+            "index": self.request.POST["word_id"],
+            "definition": self.request.POST["label"],
+        }
+
+        administered_words.append(self.word["definition"])
+        if "yes" in self.request.POST:
             administered_responses.append(True)
         else:
             administered_responses.append(False)
 
-        administered_items.append(self.word['index'])
-        
+        administered_items.append(self.word["index"])
+
         self.object.catresponse.administered_responses = administered_responses
         self.object.catresponse.administered_items = administered_items
         self.object.catresponse.administered_words = administered_words
         self.object.catresponse.save()
 
-        if len(administered_items) > 49 :
+        if len(administered_items) > 49:
             try:
-                filename = os.path.realpath(PROJECT_ROOT + self.object.study.demographic.path)
+                filename = os.path.realpath(
+                    PROJECT_ROOT + self.object.study.demographic.path
+                )
             except Exception:
-                filename = 'None'
-            if  os.path.isfile(filename):
+                filename = "None"
+            if os.path.isfile(filename):
                 self.object.completedSurvey = True
-            else :
+            else:
                 self.object.completed = True
             self.object.save()
 
-        self.request.METHOD = 'GET'
-        return redirect('cat_forms:administer_cat_form', hash_id=self.hash_id)
+        self.request.METHOD = "GET"
+        return redirect("cat_forms:administer_cat_form", hash_id=self.hash_id)
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['language_code'] = language_map(self.object.study.instrument.language)
-        
+        ctx["language_code"] = language_map(self.object.study.instrument.language)
+
         if self.word:
-            logger.debug(f'word is { self.word }')
-            ctx['form'] = CatItemForm(context={'label':self.word['definition']}, initial={'word_id':self.word['index'], 'label':self.word['definition']})
+            logger.debug(f"word is { self.word }")
+            ctx["form"] = CatItemForm(
+                context={"label": self.word["definition"]},
+                initial={
+                    "word_id": self.word["index"],
+                    "label": self.word["definition"],
+                },
+            )
             try:
-                if '*' in self.word['definition']: ctx['footnote'] = True
+                if "*" in self.word["definition"]:
+                    ctx["footnote"] = True
             except AttributeError:
                 pass
 
-        ctx['max_words'] = self.max_words
+        ctx["max_words"] = self.max_words
         try:
             if self.object.catresponse.administered_words:
-                ctx['words_shown'] = len(self.object.catresponse.administered_words) + 1
+                ctx["words_shown"] = len(self.object.catresponse.administered_words) + 1
             else:
-                ctx['words_shown'] = 1
-            ctx['est_theta'] = self.est_theta
-            ctx['due_date'] = self.object.due_date.strftime('%b %d, %Y, %I:%M %p')
-            ctx['hardest'], ctx['easiest'] = self.get_hardest_easiest()
+                ctx["words_shown"] = 1
+            ctx["est_theta"] = self.est_theta
+            ctx["due_date"] = self.object.due_date.strftime("%b %d, %Y, %I:%M %p")
+            ctx["hardest"], ctx["easiest"] = self.get_hardest_easiest()
         except:
-            #we get here if the form is expired without being opened
-            ctx['words_shown'] = 0
-            ctx['est_theta'] = self.est_theta
-            ctx['due_date'] = self.object.due_date.strftime('%b %d, %Y, %I:%M %p')
-            ctx['hardest'], ctx['easiest'] = None, None
+            # we get here if the form is expired without being opened
+            ctx["words_shown"] = 0
+            ctx["est_theta"] = self.est_theta
+            ctx["due_date"] = self.object.due_date.strftime("%b %d, %Y, %I:%M %p")
+            ctx["hardest"], ctx["easiest"] = None, None
         return ctx
 
     def get(self, request, *args, **kwargs):
-        logger.debug(f'Administer CAT Form')
+        logger.debug(f"Administer CAT Form")
         self.object = self.get_object()
-        logger.debug(f'Object: { self.object }; Completed: { self.object.completed }; CompletedSurvey: { self.object.completedSurvey }; CompletedDemographics: { self.object.completedBackgroundInfo } ')
+        logger.debug(
+            f"Object: { self.object }; Completed: { self.object.completed }; CompletedSurvey: { self.object.completedSurvey }; CompletedDemographics: { self.object.completedBackgroundInfo } "
+        )
         user_language = language_map(self.object.study.instrument.language)
-        self.language=user_language
+        self.language = user_language
         translation.activate(user_language)
-        if not self.object.completed and self.object.due_date < timezone.now(): 
-            logger.debug(f'{self.object} is not completed it is too late')
-            response = render (request, 'cdi_forms/expired.html', {}) # Render contact form template   
+        if not self.object.completed and self.object.due_date < timezone.now():
+            logger.debug(f"{self.object} is not completed it is too late")
+            response = render(
+                request, "cdi_forms/expired.html", {}
+            )  # Render contact form template
             response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
             return response
         requests_log.objects.create(url_hash=self.hash_id, request_type="GET")
@@ -156,18 +204,26 @@ class AdministerAdministraionView(UpdateView):
             logger.debug(f"Completed for {self.object} is { self.object.completed }")
             logger.debug(f"Due date for {self.object} is { self.object.due_date }")
             logger.debug(f"Administration instance {self.object} COMPLETED")
-            response = render(request, 'cdi_forms/cat_forms/cat_completed.html', context=self.get_context_data())
+            response = render(
+                request,
+                "cdi_forms/cat_forms/cat_completed.html",
+                context=self.get_context_data(),
+            )
             response.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
             return response
-        background_instance, created = BackgroundInfo.objects.get_or_create(administration=self.object) 
+        background_instance, created = BackgroundInfo.objects.get_or_create(
+            administration=self.object
+        )
         if self.object.completedSurvey:
             logger.debug(f"Administration instance {self.object} BACKPAGE")
-            return redirect('backpage-background-info', pk=background_instance.pk)
+            return redirect("backpage-background-info", pk=background_instance.pk)
         elif not self.object.completedBackgroundInfo:
             logger.debug(f"Administration instance {self.object} BACKGROUND INFO")
-            return redirect('background-info', pk=background_instance.pk)
+            return redirect("background-info", pk=background_instance.pk)
 
-        cat_response, created = CatResponse.objects.get_or_create(administration=self.object)
+        cat_response, created = CatResponse.objects.get_or_create(
+            administration=self.object
+        )
         if created or not cat_response.est_theta:
             cat_response.est_theta = self.est_theta
             cat_response.save()
@@ -177,27 +233,32 @@ class AdministerAdministraionView(UpdateView):
         administered_words = self.object.catresponse.administered_words or []
         self.est_theta = self.object.catresponse.est_theta
 
-        
-        if len(administered_words) < 1 : # first word might be specified by age
-            self.word = cdi_cat_api(f'startItem?age_mos={self.object.backgroundinfo.age}&language={CAT_LANG_DICT[self.language]}')
-        else:    
-            self.word = cdi_cat_api(f'nextItem?responses={list(map(int,administered_responses))}&items={administered_items}&language={CAT_LANG_DICT[self.language]}')
-            if self.word['stop'] == True:
-                logger.debug(f'CAT Completed { self.word } ')
+        if len(administered_words) < 1:  # first word might be specified by age
+            self.word = cdi_cat_api(
+                f"startItem?age_mos={self.object.backgroundinfo.age}&language={CAT_LANG_DICT[self.language]}"
+            )
+        else:
+            self.word = cdi_cat_api(
+                f"nextItem?responses={list(map(int,administered_responses))}&items={administered_items}&language={CAT_LANG_DICT[self.language]}"
+            )
+            if self.word["stop"] == True:
+                logger.debug(f"CAT Completed { self.word } ")
                 try:
-                    filename = os.path.realpath(PROJECT_ROOT + self.object.study.demographic.path)
+                    filename = os.path.realpath(
+                        PROJECT_ROOT + self.object.study.demographic.path
+                    )
                 except Exception:
-                    filename = 'None'
-                if  os.path.isfile(filename):
+                    filename = "None"
+                if os.path.isfile(filename):
                     self.object.completedSurvey = True
-                else :
+                else:
                     self.object.completed = True
-                self.object.catresponse.est_theta = self.word['curTheta']
+                self.object.catresponse.est_theta = self.word["curTheta"]
                 self.object.save()
                 self.object.catresponse.save()
-                return redirect('cat_forms:administer_cat_form', hash_id=self.hash_id)
-            else :
-                self.object.catresponse.est_theta = self.word['curTheta']
+                return redirect("cat_forms:administer_cat_form", hash_id=self.hash_id)
+            else:
+                self.object.catresponse.est_theta = self.word["curTheta"]
                 self.object.save()
                 self.object.catresponse.save()
-        return super().get(request, *args, **kwargs) 
+        return super().get(request, *args, **kwargs)
