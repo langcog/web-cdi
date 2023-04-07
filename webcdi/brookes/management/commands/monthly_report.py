75abfab1cf9c1d1a26f88fd15fe8be65452f049d
henrymehta <hjsmehta@gmail.com>
2023-04-07 10:22:23 +0100

    Brookes Monthly report

--- webcdi/brookes/management/commands/monthly_report.py
+++ webcdi/brookes/management/commands/monthly_report.py
@@ -1,29 +1,48 @@
 import csv
 import datetime
 
-from django.conf import settings
-from django.core.management.base import BaseCommand
-from django.core.mail import EmailMessage
-
 from brookes.models import BrookesCode
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
-    help = 'Sends monthly report to settings.BROOKES_EMAIL'
+    help = "Sends monthly report to settings.BROOKES_EMAIL"
     to_email = settings.BROOKES_EMAIL
-    
-    def handle(self, *args, **options):    
+
+    def handle(self, *args, **options):
         codes = BrookesCode.objects.all()
         filename_csv = f'{settings.MEDIA_ROOT}/Brookes Codes as of {datetime.date.today().strftime("%b-%d-%Y")}.csv'
-        with open(filename_csv, 'w', newline='') as csvfile:
+        with open(filename_csv, "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Code', 'Researcher', 'Created', 'Modified', 'Instrument Family', 'Applied'])
+            writer.writerow(
+                [
+                    "Code",
+                    "Researcher",
+                    "Created",
+                    "Modified",
+                    "Instrument Family",
+                    "Applied",
+                ]
+            )
             for code in codes:
-                writer.writerow([code.code, code.researcher, code.created, code.modified, code.instrument_family, code.applied])
-            
+                writer.writerow(
+                    [
+                        code.code,
+                        code.researcher,
+                        code.created,
+                        code.modified,
+                        code.instrument_family,
+                        code.applied,
+                    ]
+                )
+
         email_message = EmailMessage(
             subject=f"{filename_csv}",
             body=f"Please find attached monthly report {filename_csv}",
-            to=[self.to_email])
-        with open(filename_csv, 'r') as csvfile:
-            email_message.attach=[(filename_csv, csvfile.read(), 'text/csv')]
+            to=[self.to_email],
+        )
+        with open(filename_csv, "r") as csvfile:
+            email_message.attach = [(filename_csv, csvfile.read(), "text/csv")]
             email_message.send()
--- webcdi/webcdi/local_settings.py
+++ webcdi/webcdi/local_settings.py
@@ -6,121 +6,135 @@ HOST_NAME = socket.gethostname()
 HOST_IP = socket.gethostbyname(HOST_NAME)
 
 ALLOWED_HOSTS = [
-    'ec2-52-88-52-34.us-west-2.compute.amazonaws.com',
-    HOST_IP, HOST_NAME, 
-    'localhost', 
-    '127.0.0.2',
-    '127.0.0.1',
-    'webcdi-dev.us-west-2.elasticbeanstalk.com',
-    '.us-west-2.elasticbeanstalk.com',
-    'webcdi.stanford.edu', 
-    'webcdi-dev.stanford.edu', 
-    '.elb.amazonaws.com']
+    "ec2-52-88-52-34.us-west-2.compute.amazonaws.com",
+    HOST_IP,
+    HOST_NAME,
+    "localhost",
+    "127.0.0.2",
+    "127.0.0.1",
+    "webcdi-dev.us-west-2.elasticbeanstalk.com",
+    ".us-west-2.elasticbeanstalk.com",
+    "webcdi.stanford.edu",
+    "webcdi-dev.stanford.edu",
+    ".elb.amazonaws.com",
+]
 
 IPS_TO_ADD = [socket.gethostname()]
 
 NEW_IPS = set()
 
 for IP in IPS_TO_ADD:
-    for i in range(0,100):
-    	NEW_IPS.add(socket.gethostbyname(IP))
+    for i in range(0, 100):
+        NEW_IPS.add(socket.gethostbyname(IP))
 
 for IP in list(NEW_IPS):
-	ALLOWED_HOSTS.append(IP)
+    ALLOWED_HOSTS.append(IP)
 
-ADMINS = (
-    ('Henry Mehta', 'hjsmehta@gmail.com'),
-)
+ADMINS = (("Henry Mehta", "hjsmehta@gmail.com"),)
 
 
-#Database Settings
-if 'RDS_HOSTNAME' in os.environ:
+# Database Settings
+if "RDS_HOSTNAME" in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["RDS_DB_NAME"],
+            "USER": os.environ["RDS_USERNAME"],
+            "PASSWORD": os.environ["RDS_PASSWORD"],
+            "HOST": os.environ["RDS_HOSTNAME"],
+            "PORT": os.environ["RDS_PORT"],
         }
     }
-elif 'MPI_INSTANCE' in os.environ:
+elif "MPI_INSTANCE" in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE':'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['MPI_DB_NAME'],
-            'USER': os.environ['MPI_USERNAME'],
-            'PASSWORD': os.environ['MPI_PASSWORD'],
-            'HOST': os.environ['MPI_HOSTNAME'],
-            'PORT': os.environ['MPI_PORT'],
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["MPI_DB_NAME"],
+            "USER": os.environ["MPI_USERNAME"],
+            "PASSWORD": os.environ["MPI_PASSWORD"],
+            "HOST": os.environ["MPI_HOSTNAME"],
+            "PORT": os.environ["MPI_PORT"],
         }
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'webcdi-admin', 
-            'USER': 'webcdi-admin',
-            'PASSWORD': 'bears1stlexicon', 
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "webcdi-admin",
+            "USER": "webcdi-admin",
+            "PASSWORD": "bears1stlexicon",
             #'HOST': 'webcdiadmin.canyiscnpddk.us-west-2.rds.amazonaws.com',
-            'HOST': 'webcdi-dev-argentinian.canyiscnpddk.us-west-2.rds.amazonaws.com',
-            'PORT': '5432',
+            "HOST": "webcdi-dev-argentinian.canyiscnpddk.us-west-2.rds.amazonaws.com",
+            "PORT": "5432",
         }
-    }    
-    
-if 'RDS_HOSTNAME' in os.environ:
-    if os.environ['RDS_HOSTNAME'] == 'webcdiadmin.canyiscnpddk.us-west-2.rds.amazonaws.com':
-        os.environ['DJANGO_SERVER_TYPE'] = 'PROD'
-        os.environ['VANITY_URL'] = 'webcdi.stanford.edu'
+    }
+
+if "RDS_HOSTNAME" in os.environ:
+    if (
+        os.environ["RDS_HOSTNAME"]
+        == "webcdiadmin.canyiscnpddk.us-west-2.rds.amazonaws.com"
+    ):
+        os.environ["DJANGO_SERVER_TYPE"] = "PROD"
+        os.environ["VANITY_URL"] = "webcdi.stanford.edu"
         SITE_ID = 1
-    elif os.environ['RDS_HOSTNAME'] == 'webcdi-dev-1-py36.canyiscnpddk.us-west-2.rds.amazonaws.com':
-        os.environ['DJANGO_SERVER_TYPE'] = 'DEV'
+    elif (
+        os.environ["RDS_HOSTNAME"]
+        == "webcdi-dev-1-py36.canyiscnpddk.us-west-2.rds.amazonaws.com"
+    ):
+        os.environ["DJANGO_SERVER_TYPE"] = "DEV"
         SITE_ID = 2
-    else :
-        os.environ['DJANGO_SERVER_TYPE'] = 'DEV'
+    else:
+        os.environ["DJANGO_SERVER_TYPE"] = "DEV"
         SITE_ID = 2
-elif 'MPI_INSTANCE' in os.environ:
-    os.environ['DJANGO_SERVER_TYPE'] = 'PROD'
+elif "MPI_INSTANCE" in os.environ:
+    os.environ["DJANGO_SERVER_TYPE"] = "PROD"
     SITE_ID = 4
 else:
-    os.environ['DJANGO_SERVER_TYPE'] = 'DEV'
+    os.environ["DJANGO_SERVER_TYPE"] = "DEV"
     SITE_ID = 3
 
-#USER_ADMIN_EMAIL = 'webcdi-contact@stanford.edu'
-USER_ADMIN_EMAIL = 'hjsmehta@gmail.com'
+# USER_ADMIN_EMAIL = 'webcdi-contact@stanford.edu'
+USER_ADMIN_EMAIL = "hjsmehta@gmail.com"
 
-SERVER_EMAIL = 'webcdi.contact@gmail.com'
+SERVER_EMAIL = "webcdi.contact@gmail.com"
 
-#captcha settings
-RECAPTCHA_PUBLIC_KEY = '6LfI0yEUAAAAALj8wAxmoXmWg8B64tvr866bXeYg'
-RECAPTCHA_PRIVATE_KEY = '6LfI0yEUAAAAALgfbuvciUNhUCLAgslLOtnsFnx3'
+# captcha settings
+RECAPTCHA_PUBLIC_KEY = "6LfI0yEUAAAAALj8wAxmoXmWg8B64tvr866bXeYg"
+RECAPTCHA_PRIVATE_KEY = "6LfI0yEUAAAAALgfbuvciUNhUCLAgslLOtnsFnx3"
 
-#Home page links
-CONTACT_EMAIL = 'webcdi-contact@stanford.edu'
-MORE_INFO_ADDRESS = 'http://mb-cdi.stanford.edu/'
+# Home page links
+CONTACT_EMAIL = "webcdi-contact@stanford.edu"
+MORE_INFO_ADDRESS = "http://mb-cdi.stanford.edu/"
 
-#AWS creds
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '<AWS_ACCESS_KEY>')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '<AWS_SECRET_KEY')
-if 'RDS_HOSTNAME' in os.environ:
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '<AWS_ACCESS_KEY>')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '<AWS_SECRET_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME','AWS_STORAGE_BUCKET')
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS creds
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "<AWS_ACCESS_KEY>")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "<AWS_SECRET_KEY")
+if "RDS_HOSTNAME" in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "<AWS_ACCESS_KEY>")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "<AWS_SECRET_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get(
+        "AWS_STORAGE_BUCKET_NAME", "AWS_STORAGE_BUCKET"
+    )
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-#CAT Server
-CAT_API_BASE_URL = os.environ.get('CAT_API_URL',"http://cdicatapi-henry.us-west-2.elasticbeanstalk.com/")
+# CAT Server
+CAT_API_BASE_URL = os.environ.get(
+    "CAT_API_URL", "http://cdicatapi-henry.us-west-2.elasticbeanstalk.com/"
+)
 
-#Email settings
+# Email settings
 
-#EMAIL settings
-AWS_SES_REGION_NAME = 'us-west-2'
-AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
-EMAIL_BACKEND = 'django_ses.SESBackend'
-DEFAULT_FROM_EMAIL_NAME = os.environ.get('DEFAULT_FROM_EMAIL_NAME','WebCDI Local')
-DEFAULT_FROM_EMAIL_ADDRESS = os.environ.get('DEFAULT_FROM_EMAIL_ADDRESS','hjsmehta@gmail.com')
-DEFAULT_FROM_EMAIL = DEFAULT_FROM_EMAIL_NAME + '<' + DEFAULT_FROM_EMAIL_ADDRESS + '>'
-DEFAULT_RECIPIENT_EMAIL = EMAIL_HOST_USER = os.environ.get('DEFAULT_RECIPIENT_EMAIL','hjsmehta@gmail.com')
+# EMAIL settings
+AWS_SES_REGION_NAME = "us-west-2"
+AWS_SES_REGION_ENDPOINT = "email.us-west-2.amazonaws.com"
+EMAIL_BACKEND = "django_ses.SESBackend"
+DEFAULT_FROM_EMAIL_NAME = os.environ.get("DEFAULT_FROM_EMAIL_NAME", "WebCDI Local")
+DEFAULT_FROM_EMAIL_ADDRESS = os.environ.get(
+    "DEFAULT_FROM_EMAIL_ADDRESS", "hjsmehta@gmail.com"
+)
+DEFAULT_FROM_EMAIL = DEFAULT_FROM_EMAIL_NAME + "<" + DEFAULT_FROM_EMAIL_ADDRESS + ">"
+DEFAULT_RECIPIENT_EMAIL = EMAIL_HOST_USER = os.environ.get(
+    "DEFAULT_RECIPIENT_EMAIL", "hjsmehta@gmail.com"
+)
 
-BROOKES_EMAIL = os.environ.get('BROOKES_EMAIL', 'hjsmehta@gmail.com')
+BROOKES_EMAIL = os.environ.get("BROOKES_EMAIL", "hjsmehta@gmail.com")
