3a4ef083f73b6bfe9ee2b55aa74caf436f460bed
henrymehta <hjsmehta@gmail.com>
2023-04-07 12:38:10 +0100

    Researcher reminders

--- webcdi/brookes/admin.py
+++ webcdi/brookes/admin.py
@@ -1,14 +1,35 @@
+import datetime
+import csv
 from brookes.models import BrookesCode
 from django.contrib import admin, messages
+from django.conf import settings
+from django.core.mail import EmailMessage
+
 from rangefilter.filters import DateRangeFilter
 
 # Register your models here.
 
-
 def create_50_codes(modeladmin, request, queryset):
-    for count in range(50):
-        x = BrookesCode()
-        x.save()
+    filename_csv = (
+        f'/tmp/New Brookes Codes at {datetime.date.today().strftime("%b-%d-%Y")}.csv'
+    )
+    with open(filename_csv, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Code"])
+        for count in range(50):
+            x = BrookesCode()
+            x.save()
+            writer.writerow([x.code])
+
+    email_message = EmailMessage(
+        subject="New WebCDI Codes",
+        body=f"Please find attached new WebCDI Codes {filename_csv}",
+        to=[settings.BROOKES_EMAIL],
+    )
+    with open(filename_csv, "r") as csvfile:
+        email_message.attach(filename_csv, csvfile.read(), "text/csv")
+        email_message.send()
+
     messages.success(request, f"50 Codes created")
 
 
---
+++ webcdi/brookes/management/commands/weekly_reminder.py
@@ -0,0 +1,26 @@
+import csv
+import datetime
+
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+
+from dateutil.relativedelta import relativedelta
+
+from brookes.models import BrookesCode
+
+
+class Command(BaseCommand):
+    help = "Runs weekly to send to reminders to researchers whose access expires within a month"
+
+    def handle(self, *args, **options):
+        from_date = datetime.date.today() - relativedelta(years=1)
+        to_date = datetime.date.today() - relativedelta(years=1) + relativedelta(months=1)
+        codes = BrookesCode.objects.filter(applied__gte=from_date, applied__lte=to_date)
+        for code in codes:
+            email_message = EmailMessage(
+                subject=f"WebCDI {code.instrument_family} renewal is due",
+                body=f"You annual license for WebCDI {code.instrument_family} will expire on {code.applied + relativedelta(years=1)}.  Please renew your subscription.",
+                to=[code.researcher.email],
+            )
+            email_message.send()
