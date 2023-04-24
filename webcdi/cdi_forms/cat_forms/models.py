80cb59e752cfd7e1c4244364171a6d769c2f9313
henrymehta <hjsmehta@gmail.com>
2023-04-24 19:56:24 +0100

    Moving some languages

--- webcdi/cdi_forms/cat_forms/models.py
+++ webcdi/cdi_forms/cat_forms/models.py
@@ -1,11 +1,13 @@
-from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
 
 # Create your models here.
 
 
 class CatResponse(models.Model):
-    administration = models.OneToOneField('researcher_UI.administration', db_index=True, on_delete=models.CASCADE)
+    administration = models.OneToOneField(
+        "researcher_UI.administration", db_index=True, on_delete=models.CASCADE
+    )
     administered_words = ArrayField(models.CharField(max_length=101), null=True)
     administered_items = ArrayField(models.IntegerField(), null=True)
     administered_responses = ArrayField(models.BooleanField(), null=True)
@@ -13,4 +15,3 @@ class CatResponse(models.Model):
 
     def __str__(self):
         return self.administration
-
