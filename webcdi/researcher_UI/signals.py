5bc48d3971a8ddae4ff2063045264a87bcf8fe98
henrymehta <hjsmehta@gmail.com>
2023-04-28 10:12:50 +0100

    #441 Brookes Changes

--- webcdi/researcher_UI/signals.py
+++ webcdi/researcher_UI/signals.py
@@ -1,6 +1,3 @@
-from django.conf import settings
-from django.contrib.sites.models import Site
-from django.core.mail import send_mail
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
