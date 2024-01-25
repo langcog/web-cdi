6f90cb0ce8a4a0f62e672084e1256f1175938aa3
HenryMehta <hjsmehta@gmail.com>
2024-01-25 13:48:00 +0000

    requirements

--- webcdi/.platform/hooks/postdeploy/02_django.sh
+++ webcdi/.platform/hooks/postdeploy/02_django.sh
@@ -3,13 +3,13 @@
 source "$PYTHONPATH/activate" && {
     # migrate
     python ./manage.py migrate --noinput;
-    #python ./manage.py collectstatic --noinput;
-    #python ./manage.py 01_populate_instrument_family;
-    #python ./manage.py 02_populate_instrument;
-    #python ./manage.py 03_populate_scoring;
-    #python ./manage.py 04a_delete_benchmark;
-    #python ./manage.py 04_populate_benchmark;
-    #python ./manage.py 05_populate_choices;
-    #python ./manage.py 06_populate_items;
+    python ./manage.py collectstatic --noinput;
+    python ./manage.py 01_populate_instrument_family;
+    python ./manage.py 02_populate_instrument;
+    python ./manage.py 03_populate_scoring;
+    python ./manage.py 04a_delete_benchmark;
+    python ./manage.py 04_populate_benchmark;
+    python ./manage.py 05_populate_choices;
+    python ./manage.py 06_populate_items;
     #python ./manage.py 07_populate_cat_items;
 }
