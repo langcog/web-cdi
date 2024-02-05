d7ada2cc0229bef0b4ec43951b1e0d83ebc98550
HenryMehta <hjsmehta@gmail.com>
2024-02-05 09:42:24 +0000

    #502 Correct Gift Card Validation for Amazon

--- webcdi/researcher_UI/utils/raw_gift_codes.py
+++ webcdi/researcher_UI/utils/raw_gift_codes.py
@@ -2,30 +2,49 @@ import logging
 import re
 from decimal import Decimal
 
+from django.utils.safestring import mark_safe
+from django.contrib import messages
 from researcher_UI.models import payment_code
 
 logger = logging.getLogger("debug")
 
 
-def raw_gift_code_fun(raw_gift_amount, study_obj, new_study_name, raw_gift_codes):
+def raw_gift_code_fun(request, raw_gift_amount, study_obj, new_study_name, raw_gift_codes):
     all_new_codes = None
     amount_regex = None
     data = {}
     if raw_gift_codes:
-        new_payment_codes = []
-        used_codes = []
         gift_codes = re.split("[,;\s\t\n]+", raw_gift_codes)
         gift_regex = re.compile(r"^[a-zA-Z0-9]{4}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{4}$")
         gift_type = "Amazon"
-        gift_codes = filter(gift_regex.search, gift_codes)
 
+        # Get valid codes
+        valid_gift_codes = []
+        invalid_gift_codes = []
+        for gift_code in gift_codes:
+            if gift_regex.search(gift_code):
+                valid_gift_codes.append(gift_code)
+            else:
+                invalid_gift_codes.append(gift_code)
+            
+        if len(invalid_gift_codes) > 0:
+            messages.error(
+                request,
+                mark_safe(
+                    f"The following codes are invalid: {invalid_gift_codes}."
+                ),
+            )
+        
         try:
             amount_regex = Decimal(raw_gift_amount.replace("$", ""))
         except:
             pass
 
         if amount_regex:
-            for gift_code in gift_codes:
+            new_payment_codes = []
+            used_codes = []
+            new_codes = []
+            for gift_code in valid_gift_codes:
                 if not payment_code.objects.filter(
                     payment_type=gift_type, gift_code=gift_code
                 ).exists():
@@ -37,34 +56,34 @@ def raw_gift_code_fun(raw_gift_amount, study_obj, new_study_name, raw_gift_codes
                             gift_amount=amount_regex,
                         )
                     )
+                    new_codes.append(gift_code)
                 else:
                     used_codes.append(gift_code)
+            payment_code.objects.bulk_create(new_payment_codes)
+
+            if len(used_codes) > 0:
+                messages.warning(
+                    request,
+                    mark_safe(
+                        f"The following codes are previously used: {used_codes}"
+                    ),
+                )
+            if len(new_codes) > 0:
+                messages.success(
+                    request,
+                    mark_safe(
+                        f"The following codes have been added: {new_codes}"
+                    ),
+                )
+        else:
+            messages.error(
+                request,
+                mark_safe(
+                    f"The amount {raw_gift_amount} is invalid"
+                ),
+            )
 
         if not used_codes:
             all_new_codes = True
 
-    if raw_gift_codes:
-        if not all_new_codes or not amount_regex:
-            data["stat"] = "error"
-            err_msg = []
-            if not all_new_codes:
-                err_msg = (
-                    err_msg
-                    + ["The following codes are already in the database:"]
-                    + used_codes
-                )
-            if not amount_regex:
-                err_msg = err_msg + [
-                    'Please enter in a valid amount for "Amount per Card"'
-                ]
-
-            data["error_message"] = "<br>".join(err_msg)
-
-        else:
-            if all_new_codes:
-                payment_code.objects.bulk_create(new_payment_codes)
-                data["stat"] = "ok"
-    else:
-        data["stat"] = "ok"
-
-    return data
+    return
