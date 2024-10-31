f7e2606d7f017226768b6da57720c33f30a25199
henrymehta <hjsmehta@gmail.com>
2024-10-31 09:46:16 +0000

    correct error message

--- webcdi/manage.py
+++ webcdi/manage.py
@@ -10,10 +10,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webcdi.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as e:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            f"Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? Did you forget to activate a virtual environment? \n Error {e}"
+        )
     execute_from_command_line(sys.argv)
