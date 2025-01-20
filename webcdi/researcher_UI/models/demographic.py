0b6ffffe454fd4bb56f97465efadedc9403ad361
HenryMehta <hjsmehta@gmail.com>
2025-01-20 10:11:39 +0000

    Initial CRI3R

--- webcdi/researcher_UI/models/demographic.py
+++ webcdi/researcher_UI/models/demographic.py
@@ -6,7 +6,7 @@ class Demographic(models.Model):
     Class to store the different Demographic Form (Background Info) that can be used by Studies
     """
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     path = models.CharField(max_length=255)
 
     def __str__(self):
